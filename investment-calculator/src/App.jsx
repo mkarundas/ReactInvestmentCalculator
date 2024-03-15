c8d60a299f36bf496443d2ec69da8477be1b1a3b
Arundas MK <mkarundas@gmail.com>
2024-03-15 22:48:47 +0530

    adding new components and finishing project

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  
+
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(previousInput => {
+        return {
+            ...previousInput,
+            [inputIdentifier]: +newValue
+        }
+    });
+}
+
+const inputIsValid = userInput.duration >= 1;
+
+return <>
+  <Header/>
+  <UserInput userInput={userInput} onChange={handleChange}/>
+  {!inputIsValid && <p className="center">Please enter valid input data.</p>}
+  {inputIsValid && <Results input={userInput}/> }
+  </>
 }
 
 export default App
